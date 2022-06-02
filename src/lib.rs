d393e9cc58cd06a679cbc9548e68aa03bccf3765
gcxfd <i@rmw.link>
2022-06-02 15:44:57 +0800

    🔶

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ pub struct HashDepth {
 }
 
 // (1<<10) * 1024 = 1MB
-pub const BLOCK_CHUNK: u8 = 3;
+pub const BLOCK_CHUNK: u8 = 10;
 
 #[derive(Debug)]
 pub struct Merkle {
@@ -69,65 +69,72 @@ impl Merkle {
       1 => li[0].hash,
       2 => parent_cv(&li[0].hash, &li[1].hash, true),
       len => {
-        let mut n = 1;
-        while li[n].depth == BLOCK_CHUNK {
-          n += 1;
-          if n == len {
-            break;
-          }
-        }
-        if n % 2 != 0 {
-          n -= 1;
-        }
-
-        let mut hash_li: Vec<_> = li[n..].iter().rev().map(|i| i.hash).collect();
-        let mut hash_li_len = len - n;
-
-        if n != 0 {
-          let mut li: Box<[_]> = li[..n].iter().map(|i| i.hash).collect();
-          loop {
-            if n == 1 {
-              hash_li.push(li[0]);
+        let (hash_li, hash_li_len) = {
+          // 合并队列前depth为BLOCK_CHUNK的项
+          let mut n = 1;
+          while li[n].depth == BLOCK_CHUNK {
+            n += 1;
+            if n == len {
               break;
             }
-            if n % 2 != 0 {
-              n -= 1;
-              hash_li.push(li[n]);
-              hash_li_len += 1;
-            }
-            n /= 2;
-            if (n == 1) && (hash_li_len == 0) {
-              return parent_cv(&li[0], &li[1], true);
-            }
-            li = {
-              let mut box_li = unsafe { Box::<[Hash]>::new_uninit_slice(n).assume_init() };
-              let mut i = 0;
-              while i < n {
-                let t = 2 * i;
-                box_li[i] = parent_cv(&li[t], &li[t + 1], false);
-                i += 1;
+          }
+          if n % 2 != 0 {
+            n -= 1;
+          }
+
+          let mut hash_li: Vec<_> = li[n..].iter().rev().map(|i| i.hash).collect();
+          let mut hash_li_len = len - n;
+
+          if n != 0 {
+            let mut li: Box<[_]> = li[..n].iter().map(|i| i.hash).collect();
+            loop {
+              if n == 1 {
+                hash_li.push(li[0]);
+                hash_li_len += 1;
+                break;
               }
-              box_li
+              if n % 2 != 0 {
+                n -= 1;
+                hash_li.push(li[n]);
+                hash_li_len += 1;
+              }
+              n /= 2;
+              if (n == 1) && (hash_li_len == 0) {
+                return parent_cv(&li[0], &li[1], true);
+              }
+              li = {
+                let mut box_li = unsafe { Box::<[Hash]>::new_uninit_slice(n).assume_init() };
+                let mut i = 0;
+                while i < n {
+                  let t = 2 * i;
+                  box_li[i] = parent_cv(&li[t], &li[t + 1], false);
+                  i += 1;
+                }
+                box_li
+              }
             }
           }
-        }
+          (hash_li, hash_li_len)
+        };
 
-        let len = hash_li.len() - 1;
-
-        n = 0;
-        let right = &hash_li[0];
-        n += 1;
-        let mut finalize = len == n;
-
-        let mut hash = parent_cv(&hash_li[n], right, finalize);
-
-        while !finalize {
+        {
+          // 从右往左合并队列
+          let len = hash_li_len - 1;
+          let mut n = 0;
+          let right = &hash_li[0];
           n += 1;
-          finalize = len == n;
-          hash = parent_cv(&hash_li[n], &hash, finalize);
-        }
+          let mut finalize = len == n;
 
-        hash
+          let mut hash = parent_cv(&hash_li[n], right, finalize);
+
+          while !finalize {
+            n += 1;
+            finalize = len == n;
+            hash = parent_cv(&hash_li[n], &hash, finalize);
+          }
+
+          hash
+        }
       }
     }
   }
